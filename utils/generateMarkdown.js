d3bb81add728535789b87cd045fa0a357b82533f
twizard <trevcoons@gmail.com>
2021-07-14 17:31:48 -0700

    create return function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,16 +5,25 @@
 function renderLicenseBadge(license) {
   if (license !== 'none') {
     return `![license](https://shields)`
-  } return '';
+  } else return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return `![license](#license)`
+  } else return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `## License
+    the project is licensed under the ${license} license`
+  } else return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
